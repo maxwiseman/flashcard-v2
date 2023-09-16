9e0a37ab066f9416643b46923dadb9451ab19cfb
Max Wiseman <13maxwellwise@gmail.com>
2023-09-16 00:25:36 -0400

    Added buttons to switch between flashcards

--- src/app/cards/set/[...id]/page.tsx
+++ src/app/cards/set/[...id]/page.tsx
@@ -3,11 +3,13 @@
 import { Separator } from '@/components/ui/separator'
 import { Flashcard } from './flashcard'
 import { Button } from '@/components/ui/button'
-import { Pencil } from 'lucide-react'
+import { ArrowLeft, ArrowRight, Pencil } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { useRef } from 'react'
 
 export default function Page({ params }: { params: { id: string } }) {
   const { setTheme, theme } = useTheme()
+  const scroll = useRef(null)
 
   return (
     <>
@@ -24,15 +26,42 @@ export default function Page({ params }: { params: { id: string } }) {
       <Separator className='my-6' />
       <div
         // className='overflow-x-scroll overflow-y-hidden whitespace-nowrap w-full snap-x snap-mandatory snap-center'
-        className='flex flex-none overflow-auto snap-x snap-mandatory w-1/2 no-scrollbar'
+        className='w-1/2 h-max relative'
       >
-        {exampleSet.map((card, i) => {
-          return (
-            <div key={i} className='min-w-full relative snap-center'>
-              <Flashcard card={card} index={i} />
-            </div>
-          )
-        })}
+        <div
+          className='flex flex-none overflow-auto snap-x snap-mandatory w-full no-scrollbar relative scroll-smooth'
+          ref={scroll}
+        >
+          {exampleSet.map((card, i) => {
+            return (
+              <div key={i} className='min-w-full relative snap-center'>
+                <Flashcard card={card} index={i} />
+              </div>
+            )
+          })}
+        </div>
+        <Button
+          variant={'outline'}
+          size={'icon'}
+          className='absolute translate-y-[-50%] top-1/2 left-4'
+          onClick={() => {
+            // @ts-ignore
+            scroll?.current?.scrollBy({ left: -100, type: 'smooth' })
+          }}
+        >
+          <ArrowLeft className='w-4 h-4' />
+        </Button>
+        <Button
+          variant={'outline'}
+          size={'icon'}
+          className='absolute translate-y-[-50%] top-1/2 right-4'
+          onClick={() => {
+            // @ts-ignore
+            scroll?.current?.scrollBy({ left: 100, type: 'smooth' })
+          }}
+        >
+          <ArrowRight className='w-4 h-4' />
+        </Button>
       </div>
     </>
   )
